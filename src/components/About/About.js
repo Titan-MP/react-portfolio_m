84d86d2d20e16740f10a7834615d2d7233ac237f
Titan-MP <marcrhymaun@knights.ucf.edu>
2023-10-03 15:51:13 -0400

    working on gh pages

--- src/components/About/About.js
+++
@@ -1,9 +0,0 @@
-import './About.css';
-
-const About = () =>
-    <div className='about'>
-            <h1>About Me </h1>
-    </div>
-
-
-export default About;
---
+++ src/components/About/About.jsx
@@ -0,0 +1,17 @@
+import './About.css';
+
+const About = () =>
+    <div className='about'>
+        <div>
+            <img src='https://via.placeholder.com/150' alt='placeholder' />
+            <h1>About Me </h1>
+        </div>
+        <div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae similique enim iure quos pariatur. Excepturi, doloribus odio inventore ipsa tempora beatae placeat est doloremque dolore libero illo fugit sint quis?</p>
+            
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae similique enim iure quos pariatur. Excepturi, doloribus odio inventore ipsa tempora beatae placeat est doloremque dolore libero illo fugit sint quis?</p>
+        </div>
+    </div>
+
+
+export default About;
